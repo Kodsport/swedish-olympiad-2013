2555430d7c4cc4595820e5d6b8c205e11a09ceef
Erik Adebahr <erikadebahr@gmail.com>
2024-05-30 22:30:31 +0200

    [rakneord] skapa enkel generator och en till test grupp

--- final/rakneord/data/generator.sh
+++ final/rakneord/data/generator.sh
@@ -2,7 +2,8 @@
 . ../../../testdata_tools/gen.sh
 
 
-use_solution ordnummer_fusk.py
+use_solution rakneord.cpp
+compile gen_random.py
 
 samplegroup
 sample 1
@@ -10,62 +11,42 @@ sample 2
 sample 3
 
 
-group 01_group01 10
+group 1_small 15
+limits n=2000
 tc_manual ../manual-tests/g01.in
+tc small-1 gen_random n=2000
+tc small-2 gen_random n=2000
+tc small-3 gen_random n=2000
+tc small-4 gen_random n=2000
+tc small-5 gen_random n=2000
 tc 1
 tc 2
 tc 3
 
-group 02_group02 10
+group 2_medium 20
+limits n=500000
+include_group 1_small
 tc_manual ../manual-tests/g02.in
-tc 1
-tc 2
-tc 3
-
-group 03_group03 10
 tc_manual ../manual-tests/g03.in
-tc 1
-tc 2
-tc 3
+tc medium-1 gen_random n=500000
+tc medium-2 gen_random n=500000
+tc medium-3 gen_random n=500000
+tc medium-4 gen_random n=500000
+tc medium-5 gen_random n=500000
 
-group 04_group04 10
+group 3_large 65
+limits n=999999999999
+include_group 2_medium
 tc_manual ../manual-tests/g04.in
-tc 1
-tc 2
-tc 3
-
-group 05_group05 10
 tc_manual ../manual-tests/g05.in
-tc 1
-tc 2
-tc 3
-
-group 06_group06 10
 tc_manual ../manual-tests/g06.in
-tc 1
-tc 2
-tc 3
-
-group 07_group07 10
 tc_manual ../manual-tests/g07.in
-tc 1
-tc 2
-tc 3
-
-group 08_group08 10
 tc_manual ../manual-tests/g08.in
-tc 1
-tc 2
-tc 3
-
-group 09_group09 10
 tc_manual ../manual-tests/g09.in
-tc 1
-tc 2
-tc 3
-
-group 10_group10 10
 tc_manual ../manual-tests/g10.in
-tc 1
-tc 2
-tc 3
+tc large-1 gen_random n=999999999999
+tc large-2 gen_random n=999999999999
+tc large-3 gen_random n=999999999999
+tc large-4 gen_random n=999999999999
+tc large-5 gen_random n=999999999999
+
