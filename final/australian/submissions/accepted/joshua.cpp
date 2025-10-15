3ea8d8b8925b49d9a5f14ce9c6940cfa4f5e941e
Matistjati <matistjati@outlook.com>
2025-10-15 20:06:20 +0000

    [australian] cleaner judge solution

--- final/australian/submissions/accepted/joshua.cpp
+++ final/australian/submissions/accepted/joshua.cpp
@@ -1,53 +1,17 @@
 #include <bits/stdc++.h>
-
 using namespace std;
 
-#pragma GCC target ("avx2")
-#pragma GCC optimization ("O3")
-#pragma GCC optimization ("unroll-loops")
+using ll = long long;
+using vi = vector<ll>;
+using vvi = vector<vi>;
+using p2 = pair<ll, ll>;
+const ll inf = 1e18;
 
-#define ll long long
-#define vi vector<ll>
-#define vvi vector<vi>
-#define p2 pair<ll, ll>
-#define p3 vi
-#define p4 vi
-#define vp2 vector<p2>
-#define vp3 vector<p3>
-#define inf 2e9
-#define linf 1e17
-
-#define read(a) cin >> a
-#define dread(type, a) type a; cin >> a
-#define dread2(type, a, b) dread(type, a); dread(type, b)
-#define dread3(type, a, b, c) dread(type, a); dread(type, b); dread(type, c)
-#define write(a) cout << (a) << endl
-#ifdef _DEBUG
-#define deb __debugbreak();
-#else
-#define deb ;
-#endif
-
-#define rep(i, high) for (ll i = 0; i < high; i++)
-#define repe(i, container) for (auto& i : container)
-#define per(i, high) for (ll i = high; i >= 0; i--)
-
-#define readpush(type,vect) type temp; read(temp); vect.push_back(temp);
-#define readvector(type, name, size) vector<type> name(size); rep(i,n) {dread(type,temp); name[i]=temp;}
-#define readinsert(type,a) {type temp; read(temp); a.insert(temp);}
-#define setcontains(set, x) (set.find(x) != set.end())
-#define stringcontains(str, x) (str.find(x) != string::npos)
-#define all(a) begin(a),end(a)
-
-#define ceildiv(x,y) ((x + y - 1) / y)
-#define fract(a) (a-floor(a))
-
-
-inline void fast()
-{
-    ios::sync_with_stdio(false);
-    cin.tie(NULL); cout.tie(NULL);
-}
+#define rep(i,n) for (ll i = 0; i < (n); i++)
+#define repp(i,a,n) for (ll i = (a); i < (n); i++)
+#define repe(i, arr) for (auto& i : arr)
+#define all(x) begin(x),end(x)
+#define sz(x) ((ll)(x).size())
 
 map<int, pair<double, double>> winningProb(vector<pair<int,double>>& strengths)
 {
@@ -68,11 +32,11 @@ map<int, pair<double, double>> winningProb(vector<pair<int,double>>& strengths)
     {
         repe(j, top)
         {
-            if (!setcontains(ret, i.first))
+            if (!ret.count(i.first))
             {
                 ret[i.first] = { 0, i.second.second };
             }
-            if (!setcontains(ret, j.first))
+            if (!ret.count(j.first))
             {
                 ret[j.first] = { 0, j.second.second };
             }
@@ -86,18 +50,13 @@ map<int, pair<double, double>> winningProb(vector<pair<int,double>>& strengths)
 
 int main()
 {
-    fast();
-
-#if 0
-    //ifstream cin("C:\\Users\\Matis\\source\\repos\\Comp prog\\x64\\Debug\\in.txt");
-    ifstream cin("C:\\Users\\Matis\\Downloads\\pixel\\examples\\sample01.in");
-#endif
-
-    dread(int, n);
+    int n;
+    cin >> n;
     vector<pair<int, double>> strengths(pow(2,n));
     rep(i, pow(2, n))
     {
-        dread(double, strength);
+        double strength;
+        cin >> strength;
         strengths[i] = { i,strength };
     }
 
@@ -117,5 +76,4 @@ int main()
     cout << bestPlayer << " " << fixed << setprecision(20) << bestProb << endl;
 
     return 0;
-    //_Exit(0);
 }
--- final/australian/submissions/partially_accepted/small.cpp
+++ final/australian/submissions/partially_accepted/small.cpp
@@ -1,59 +1,22 @@
 #include <bits/stdc++.h>
-
 using namespace std;
 
-#pragma GCC target ("avx2")
-#pragma GCC optimization ("O3")
-#pragma GCC optimization ("unroll-loops")
+using ll = long long;
+using vi = vector<ll>;
+using vvi = vector<vi>;
+using p2 = pair<ll, ll>;
+const ll inf = 1e18;
 
-#define ll long long
-#define vi vector<ll>
-#define vvi vector<vi>
-#define p2 pair<ll, ll>
-#define p3 vi
-#define p4 vi
-#define vp2 vector<p2>
-#define vp3 vector<p3>
-#define inf 2e9
-#define linf 1e17
-
-#define read(a) cin >> a
-#define dread(type, a) type a; cin >> a
-#define dread2(type, a, b) dread(type, a); dread(type, b)
-#define dread3(type, a, b, c) dread(type, a); dread(type, b); dread(type, c)
-#define write(a) cout << (a) << endl
-#ifdef _DEBUG
-#define deb __debugbreak();
-#else
-#define deb ;
-#endif
-
-#define rep(i, high) for (ll i = 0; i < high; i++)
-#define repe(i, container) for (auto& i : container)
-#define per(i, high) for (ll i = high; i >= 0; i--)
-
-#define readpush(type,vect) type temp; read(temp); vect.push_back(temp);
-#define readvector(type, name, size) vector<type> name(size); rep(i,n) {dread(type,temp); name[i]=temp;}
-#define readinsert(type,a) {type temp; read(temp); a.insert(temp);}
-#define setcontains(set, x) (set.find(x) != set.end())
-#define stringcontains(str, x) (str.find(x) != string::npos)
-#define all(a) begin(a),end(a)
-
-#define ceildiv(x,y) ((x + y - 1) / y)
-#define fract(a) (a-floor(a))
-
-
-inline void fast()
-{
-    ios::sync_with_stdio(false);
-    cin.tie(NULL); cout.tie(NULL);
-}
+#define rep(i,n) for (ll i = 0; i < (n); i++)
+#define repp(i,a,n) for (ll i = (a); i < (n); i++)
+#define repe(i, arr) for (auto& i : arr)
+#define all(x) begin(x),end(x)
+#define sz(x) ((ll)(x).size())
 
 int main()
 {
-    fast();
-
-    dread(int, n);
+    int n;
+    cin >> n;
     double a,b;
     cin >> a >> b;
 
